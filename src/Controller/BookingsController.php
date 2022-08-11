e0b816ba167394f42484fd508ddf2b34f22e558b
WEI LIU <wliu0025@student.monash.edu>
2022-08-11 16:33:52 +1000

    calendar feature finished

--- src/Controller/BookingsController.php
+++ src/Controller/BookingsController.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 namespace App\Controller;
+use Cake\Mailer\Mailer;
 
 
 /**
@@ -147,6 +148,65 @@ class BookingsController extends AppController
             if ($this->Bookings->save($booking)) {
                 $this->Flash->success(__('The booking has been saved.'));
 
+
+                // 1.Send email to client who made a booking
+                $mailer = new Mailer('default');
+                // Setup email parameters
+                $mailer
+                    ->setEmailFormat('html')
+                    ->setTo($booking->email)
+                    ->setFrom("wliu0025@u22s1043.monash-ie.me")
+                    ->setSubject('Confirmation of your booking' . " <" . h($booking->email) . ">")
+                    ->viewBuilder()
+                    ->disableAutoLayout()
+                    ->setTemplate('booking');
+
+                // Send data to the email template
+                $mailer->setViewVars([
+                    'date' => $booking->date,
+                    'booked_time' => $booking->booked_time,
+                    'service'=>$booking->service,
+                    'email' => $booking->email,
+                    'name' => $booking->name,
+                    'location' => $booking->location,
+                    'phone' => $booking->phone
+                ]);
+                //Send email
+                $email_result = $mailer->deliver();
+
+
+                // 2.Send email to Leonie
+//                $mailer = new Mailer('default');
+//                // Setup email parameters
+//                $mailer
+//                    ->setEmailFormat('html')
+//                    ->setTo($booking->email)
+//                    ->setFrom("wliu0025@u22s1043.monash-ie.me")
+//                    ->setSubject('You have new booking' . " <" . h("Leonie@shelbournelegal.com.au") . ">")
+//                    ->viewBuilder()
+//                    ->disableAutoLayout()
+//                    ->setTemplate('booking');
+//
+//                // Send data to the email template
+//                $mailer->setViewVars([
+//                    'date' => $booking->date,
+//                    'booked_time' => $booking->booked_time,
+//                    'service'=>$booking->service,
+//                    'email' => $booking->email,
+//                    'name' => $booking->name,
+//                    'location' => $booking->location,
+//                    'phone' => $booking->phone
+//                ]);
+//                //Send email
+//                $email_result = $mailer->deliver();
+
+
+
+                if ($email_result) {
+                    $this->Flash->success(__('The enquiry has been saved and sent via email.'));
+                } else {
+                    $this->Flash->error(__('Email failed to send. Please check the query in the system later. '));
+                }
                 return $this->redirect(['action' => 'index']);
             }
             $this->Flash->error(__('The booking could not be saved. Please, try again.'));
