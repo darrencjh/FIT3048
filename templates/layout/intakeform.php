28ba34d8b522d10510a8fc87be5c8752543230fc
WEI LIU <wliu0025@student.monash.edu>
2022-08-15 19:45:03 +1000

    create and import intakeform css,he intake page cleaner

--- templates/layout/intakeform.php
+++ templates/layout/intakeform.php
@@ -28,7 +28,7 @@ $intakeformDescription = 'Intake Form -  Shelbourne legal';
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 
-    <?= $this->Html->css(['normalize.min', 'bootstrap']) ?>
+    <?= $this->Html->css(['normalize.min', 'bootstrap','intakeform']) ?>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
