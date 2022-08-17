401f5514918a8a89f947ed322e0c61ae65f142db
Darren Jun Hoe Chan <dcha0031@student.monash.edu>
2022-08-17 12:58:59 +1000

    Fixed tooltip issue

--- templates/Clients/intakeform.php
+++ templates/Clients/intakeform.php
@@ -171,10 +171,11 @@
                 <div class="ff-el-group">
                     <div class="label">
                         <label>Will</label>
-<!--                        <i class="fa-solid fa-circle-question"</i>-->
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="15px" width="15px">
-                            <path d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zM262.655 90c-54.497 0-89.255 22.957-116.549 63.758-3.536 5.286-2.353 12.415 2.715 16.258l34.699 26.31c5.205 3.947 12.621 3.008 16.665-2.122 17.864-22.658 30.113-35.797 57.303-35.797 20.429 0 45.698 13.148 45.698 32.958 0 14.976-12.363 22.667-32.534 33.976C247.128 238.528 216 254.941 216 296v4c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12v-1.333c0-28.462 83.186-29.647 83.186-106.667 0-58.002-60.165-102-116.531-102zM256 338c-25.365 0-46 20.635-46 46 0 25.364 20.635 46 46 46s46-20.636 46-46c0-25.365-20.635-46-46-46z"/>
-                        </svg>
+                        <a class="d-inline-block" data-bs-toggle="tooltip" data-bs-original-title="A will is a legal document that spells out your wishes regarding the care of your children, as well as the distribution of your assets after your death." data-bs-placement="right">
+                            <svg class="label" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="15px" width="15px">
+                                <path d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zM262.655 90c-54.497 0-89.255 22.957-116.549 63.758-3.536 5.286-2.353 12.415 2.715 16.258l34.699 26.31c5.205 3.947 12.621 3.008 16.665-2.122 17.864-22.658 30.113-35.797 57.303-35.797 20.429 0 45.698 13.148 45.698 32.958 0 14.976-12.363 22.667-32.534 33.976C247.128 238.528 216 254.941 216 296v4c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12v-1.333c0-28.462 83.186-29.647 83.186-106.667 0-58.002-60.165-102-116.531-102zM256 338c-25.365 0-46 20.635-46 46 0 25.364 20.635 46 46 46s46-20.636 46-46c0-25.365-20.635-46-46-46z"/>
+                            </svg>
+                        </a>
                     </div>
                     <div class="input" style="font-size: 0;">
                         <div class="ff-el-form-check">
@@ -269,7 +270,7 @@
                 </div>
             </div>
         </div>
-		
+
         <!-- add 1.3 div  -->
         <div class="ff-el-group">
             <h4>1.3 Health Concerns</h4>
@@ -296,7 +297,7 @@
                     </div>
                 </div>
             </div>
-        </div>		
+        </div>
 
 		<!-- add family div  -->
         <div class="ff-el-group">
@@ -337,7 +338,7 @@
                 </div>
             </div>
         </div>
-		
+
 		<!-- add Children div  -->
         <div class="ff-el-group">
             <h2>Do you have children</h2>
@@ -383,9 +384,9 @@
                     </div>
                 </div>
             </div>
-        </div>		
-				
-		
+        </div>
+
+
         <div class="last">
             <div class="g-recaptcha"
                  data-sitekey="<?= RECAPTCHAV2_SITEKEY ?>">
--- templates/layout/intakeform.php
+++ templates/layout/intakeform.php
@@ -51,3 +51,11 @@ $intakeformDescription = 'Intake Form -  Shelbourne legal';
 
 </body>
 </html>
+
+<script>
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+</script>
